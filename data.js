69204a44d8ec40b5e4fa75fa1812bf1676caabe9
esbalabhakti-arch <esbalabhakti@gmail.com>
2026-08-16 21:07:26 -0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,113 @@
+// Album media list.
+// To add more media later:
+// 1. Put the file inside the media/ folder.
+// 2. Add one entry below using the same format.
+// Keep filenames simple (letters, numbers, hyphens/underscores) for GitHub Pages.
+
+const albumData = [
+  {
+    "type": "image",
+    "src": "media/01_image.jpeg",
+    "time": "11:52 AM"
+  },
+  {
+    "type": "image",
+    "src": "media/02_image.jpeg",
+    "time": "11:53 AM"
+  },
+  {
+    "type": "image",
+    "src": "media/03_image.jpeg",
+    "time": "1:02 PM"
+  },
+  {
+    "type": "image",
+    "src": "media/04_image.jpeg",
+    "time": "1:02 PM"
+  },
+  {
+    "type": "video",
+    "src": "media/05_video.mp4",
+    "time": "1:10 PM"
+  },
+  {
+    "type": "video",
+    "src": "media/06_video.mp4",
+    "time": "1:22 PM"
+  },
+  {
+    "type": "video",
+    "src": "media/07_video.mp4",
+    "time": "1:25 PM"
+  },
+  {
+    "type": "video",
+    "src": "media/08_video.mp4",
+    "time": "2:19 PM"
+  },
+  {
+    "type": "image",
+    "src": "media/09_image.jpeg",
+    "time": "2:37 PM"
+  },
+  {
+    "type": "video",
+    "src": "media/10_video.mp4",
+    "time": "2:40 PM"
+  },
+  {
+    "type": "image",
+    "src": "media/11_image.jpeg",
+    "time": "2:41 PM"
+  },
+  {
+    "type": "video",
+    "src": "media/12_video.mp4",
+    "time": "2:58 PM"
+  },
+  {
+    "type": "video",
+    "src": "media/13_video.mp4",
+    "time": "3:00 PM"
+  },
+  {
+    "type": "video",
+    "src": "media/14_video.mp4",
+    "time": "3:05 PM"
+  },
+  {
+    "type": "video",
+    "src": "media/15_video.mp4",
+    "time": "3:05 PM"
+  },
+  {
+    "type": "image",
+    "src": "media/16_image.jpeg",
+    "time": "3:06 PM"
+  },
+  {
+    "type": "image",
+    "src": "media/17_image.jpeg",
+    "time": "3:06 PM"
+  },
+  {
+    "type": "video",
+    "src": "media/18_video.mp4",
+    "time": "3:07 PM"
+  },
+  {
+    "type": "image",
+    "src": "media/19_image.jpeg",
+    "time": "3:11 PM"
+  },
+  {
+    "type": "image",
+    "src": "media/20_image.jpeg",
+    "time": "3:12 PM"
+  },
+  {
+    "type": "image",
+    "src": "media/21_image.jpeg",
+    "time": "3:14 PM"
+  }
+];
